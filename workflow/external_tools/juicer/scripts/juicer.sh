605df400caa574e02846ef63e4f0aed262485054
mahajrod <mahajrod@gmail.com>
2023-06-11 03:08:06 +0200

    Initial commit

--- workflow/external_tools/juicer/scripts/juicer.sh
+++ workflow/external_tools/juicer/scripts/juicer.sh
@@ -381,7 +381,7 @@ then
 		if [ ! $splitdirexists ]
 		then
 		    echo "(-: Created $splitdir."
-		    ln -s ${fastqdir} ${splitdir}/.
+		    ln -s ${fastqdir}/* ${splitdir}/.
 		else
 		    echo -e "---  Using already created files in $splitdir\n"
 		fi

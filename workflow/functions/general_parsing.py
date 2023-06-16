39173b46638c0376f82fe98d2a233709e2ae5a1b
mahajrod <mahajrod@gmail.com>
2023-06-16 13:49:13 +0200

    Initial commit

--- workflow/functions/general_parsing.py
+++ workflow/functions/general_parsing.py
@@ -129,7 +129,7 @@ def get_input_assemblies(input_folder_path, ploidy, fasta_extention):
                 print(filename.name)
                 print(filename.name[:-len(suffix)])
                 print(suffix)
-                if filename.name[:-len(suffix)] == suffix:
+                if filename.name[-len(suffix):] == suffix:
 
                     fasta_dict[haplotype] = filename.name
                     break

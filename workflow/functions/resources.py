24b9e044949abb36343f2eb2092ac4d9a614ffda
mahajrod <mahajrod@gmail.com>
2025-05-19 00:42:43 +0200

    Added simulate_pcr tool for insilico PCR

--- workflow/functions/resources.py
+++ workflow/functions/resources.py
@@ -17,8 +17,10 @@ def get_memory(wildcards, attempt, start_mem, coeff=2, mode="linear"):
     if mode == "exp":
         if coeff <= 1:
             raise ValueError(f"ERROR!!! Coefficient for exponential resource selection should be above 1 and not {coeff}!")
+        print([attempt, start_mem, coeff, mode])
         return int((coeff ** (attempt - 1)) * start_mem)
     elif mode == "linear":
+        print([attempt, start_mem, coeff, mode])
         return attempt * start_mem
     else:
         raise ValueError("ERROR!!! Unknown mode for memory selection")

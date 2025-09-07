bcffca1bdfcd7d49fa7df46ef65666a06f42374e
mahajrod <mahajrod@gmail.com>
2025-09-07 20:04:19 +0200

    Added simulate_pcr tool for insilico PCR

--- workflow/scripts/backup/gather_data_for_backup.py
+++ workflow/scripts/backup/gather_data_for_backup.py
@@ -25,6 +25,7 @@ results_dir_path = Path(args.results_dir)
 backup_dir_path = results_dir_path / "backup/"
 os.makedirs(backup_dir_path, exist_ok=True)
 
+"""
 # backup QC
 qc_dir_path = results_dir_path / "qc/"
 print("Backuping QC data...")
@@ -46,7 +47,20 @@ for folder_path in kmer_dir_path.glob("*"):
 
     print("Backuping kmer data...")
     print(f"\tCopying {histo_file_path}...")
-    shutil.copy(histo_file_path, backup_kmer_datatype_path)
+    os.system(f"cp -r {histo_file_path} {backup_kmer_datatype_path}")
+    #shutil.copy(histo_file_path, backup_kmer_datatype_path)
     print(f"\tCopying {genomescope_dir_path}...")
     os.system(f"cp -r {genomescope_dir_path} {backup_kmer_datatype_path}")
+"""
+# backup contamination scan
 
+contamination_scan_dir_path = results_dir_path / "contamination_scan/kraken2/"
+for folder_path in contamination_scan_dir_path.glob("*"):
+    scan_datatype = folder_path.name
+    kraken2_report_path = contamination_scan_dir_path / "contamination_scan/kraken2/" / scan_datatype / "kraken2.nt.report"
+
+    backup_scan_datatype_path = backup_dir_path / "contamination_scan/kraken2/" / scan_datatype
+    os.makedirs(backup_scan_datatype_path, exist_ok=True)
+    print("Backuping contamination scan data...")
+    print(f"\tCopying {kraken2_report_path}...")
+    os.system(f"cp -r {kraken2_report_path} {backup_scan_datatype_path}")

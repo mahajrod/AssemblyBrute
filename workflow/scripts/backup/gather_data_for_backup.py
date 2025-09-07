737f4f7378ae792a91196da536498f4c5d6e1a53
mahajrod <mahajrod@gmail.com>
2025-09-07 23:01:34 +0200

    Added simulate_pcr tool for insilico PCR

--- workflow/scripts/backup/gather_data_for_backup.py
+++ workflow/scripts/backup/gather_data_for_backup.py
@@ -1,90 +1,11 @@
 #!/usr/bin/env python
 __author__ = 'mahajrod'
 import os
-import sys
-import shutil
 import argparse
-
 from pathlib import Path
-import pandas as pd
-from RouToolPa.GeneralRoutines import FileRoutines
 
 
-parser = argparse.ArgumentParser()
-
-
-parser.add_argument("-r", "--results_dir", action="store", dest="results_dir", required=True,
-                    help="Directory containing results of the pipeline")
-
-
-args = parser.parse_args()
-
-results_dir_path = Path(args.results_dir)
-
-
-backup_dir_path = results_dir_path / "backup/"
-os.makedirs(backup_dir_path, exist_ok=True)
-
-"""
-# backup QC
-qc_dir_path = results_dir_path / "qc/"
-print("Backuping QC data...")
-print(f"\tCopying {qc_dir_path}...")
-
-os.system(f"cp -r {qc_dir_path} {backup_dir_path}")
-#shutil.copy(qc_dir_path, backup_dir)
-
-# backup kmer
-kmer_dir_path = results_dir_path / "kmer/"
-print("Backuping kmer data...")
-for folder_path in kmer_dir_path.glob("*"):
-    kmer_datatype = folder_path.name
-    filtered_kmer_dir_path = kmer_dir_path / kmer_datatype / "filtered/"
-    histo_file_path = list(filtered_kmer_dir_path.glob("*.histo"))[0]
-    genomescope_dir_path = list(filtered_kmer_dir_path.glob("genomescope"))[0]
-
-    backup_kmer_datatype_path = backup_dir_path / "kmer/" / kmer_datatype / "filtered/"
-    os.makedirs(backup_kmer_datatype_path, exist_ok=True)
-
-    print(f"\tCopying {histo_file_path}...")
-    os.system(f"cp -r {histo_file_path} {backup_kmer_datatype_path}")
-    #shutil.copy(histo_file_path, backup_kmer_datatype_path)
-    print(f"\tCopying {genomescope_dir_path}...")
-    os.system(f"cp -r {genomescope_dir_path} {backup_kmer_datatype_path}")
-
-# backup contamination scan
-print("Backuping contamination scan data...")
-contamination_scan_dir_path = results_dir_path / "contamination_scan/kraken2/"
-for folder_path in contamination_scan_dir_path.glob("*"):
-    scan_datatype = folder_path.name
-    print(scan_datatype)
-    kraken2_report_path = contamination_scan_dir_path  / scan_datatype / "kraken2.nt.report"
-
-    backup_scan_datatype_path = backup_dir_path  / "contamination_scan/kraken2/" / scan_datatype
-    os.makedirs(backup_scan_datatype_path, exist_ok=True)
-    print(f"\tCopying {kraken2_report_path}...")
-    os.system(f"cp -r {kraken2_report_path} {backup_scan_datatype_path}")
-
-# backup error corrected hifi reads
-print("Backuping corrected hifi reads data...")
-
-error_correction_read_dir_path = results_dir_path / "error_correction/"
-if error_correction_read_dir_path.exists():
-    print("\tError correction read dir exists...")
-    hifi_read_dir_path = backup_dir_path / "data/hifi/"
-    os.makedirs(hifi_read_dir_path, exist_ok=True)
-    print(f"\tCopying {error_correction_read_dir_path }...")
-    os.system(f"cp -r {error_correction_read_dir_path } {hifi_read_dir_path}")
-
-else:
-    print("\tError correction read dir was not found, skipping...")
-"""
-# backup contig stage
-
-
-#contig_dir_path = results_dir_path / "contig/"
-
-def copy_stage_file(stage_name, results_path, backup_path, file_pattern_list):
+def backup_stage_files(stage_name, results_path, backup_path, file_pattern_list):
     print(f"Backuping data from the {stage_name} stage...")
     stage_dir_path = results_path / stage_name
     if stage_dir_path.exists():
@@ -114,7 +35,7 @@ def copy_stage_file(stage_name, results_path, backup_path, file_pattern_list):
                     os.makedirs(backup_assembly_qc_stage_option_dir_path, exist_ok=True)
 
                     for filepath in assembly_qc_dir_path.glob("*") :
-                        print(filepath.name)
+                        #print(filepath.name)
                         if filepath.name == "merqury":
                             print("\t\tCopying merqury files...")
                             backup_merqury_qc_path = backup_assembly_qc_stage_option_dir_path / "merqury/"
@@ -144,10 +65,83 @@ def copy_stage_file(stage_name, results_path, backup_path, file_pattern_list):
     else:
         print(f"\t{stage_name} dir was not found, skipping...")
 
-copy_stage_file("contig", results_dir_path, backup_dir_path,
-                ["*.fasta", "*.gfa", "*.bed", "*.len", "*.cov", "*.lencov", "*.ids", "telomere"])
 
-copy_stage_file("hic_scaffolding", results_dir_path, backup_dir_path,
-                ["*.fasta", "*.bed", "*.len", "*.assembly", "*.agp", "*.ids", "*.hic", "telomere",
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-r", "--results_dir", action="store", dest="results_dir", required=True,
+                    help="Directory containing results of the pipeline")
+
+
+args = parser.parse_args()
+
+results_dir_path = Path(args.results_dir)
+
+backup_dir_path = results_dir_path / "backup/"
+os.makedirs(backup_dir_path, exist_ok=True)
+
+# backup QC
+qc_dir_path = results_dir_path / "qc/"
+print("Backuping QC data...")
+if qc_dir_path.exists():
+    print(f"\tCopying {qc_dir_path}...")
+
+    os.system(f"cp -r {qc_dir_path} {backup_dir_path}")
+else:
+    print("\tQC data not found, skipping...")
+
+# backup kmer
+kmer_dir_path = results_dir_path / "kmer/"
+print("Backuping kmer data...")
+if kmer_dir_path.exists():
+    for folder_path in kmer_dir_path.glob("*"):
+        kmer_datatype = folder_path.name
+        filtered_kmer_dir_path = kmer_dir_path / kmer_datatype / "filtered/"
+        histo_file_path = list(filtered_kmer_dir_path.glob("*.histo"))[0]
+        genomescope_dir_path = list(filtered_kmer_dir_path.glob("genomescope"))[0]
+
+        backup_kmer_datatype_path = backup_dir_path / "kmer/" / kmer_datatype / "filtered/"
+        os.makedirs(backup_kmer_datatype_path, exist_ok=True)
+
+        print(f"\tCopying {histo_file_path}...")
+        os.system(f"cp -r {histo_file_path} {backup_kmer_datatype_path}")
+        #shutil.copy(histo_file_path, backup_kmer_datatype_path)
+        print(f"\tCopying {genomescope_dir_path}...")
+        os.system(f"cp -r {genomescope_dir_path} {backup_kmer_datatype_path}")
+else:
+    print("\tKmer data not found, skipping...")
+
+# backup contamination scan
+print("Backuping contamination scan data...")
+contamination_scan_dir_path = results_dir_path / "contamination_scan/kraken2/"
+if contamination_scan_dir_path.exists():
+    for folder_path in contamination_scan_dir_path.glob("*"):
+        scan_datatype = folder_path.name
+        print(scan_datatype)
+        kraken2_report_path = contamination_scan_dir_path  / scan_datatype / "kraken2.nt.report"
+
+        backup_scan_datatype_path = backup_dir_path  / "contamination_scan/kraken2/" / scan_datatype
+        os.makedirs(backup_scan_datatype_path, exist_ok=True)
+        print(f"\tCopying {kraken2_report_path}...")
+        os.system(f"cp -r {kraken2_report_path} {backup_scan_datatype_path}")
+else:
+    print("\tContamination scan data not found, skipping...")
+# backup error corrected hifi reads
+print("Backuping corrected hifi reads data...")
+
+error_correction_read_dir_path = results_dir_path / "error_correction/"
+if error_correction_read_dir_path.exists():
+    hifi_read_dir_path = backup_dir_path / "data/hifi/"
+    os.makedirs(hifi_read_dir_path, exist_ok=True)
+    print(f"\tCopying {error_correction_read_dir_path }...")
+    os.system(f"cp -r {error_correction_read_dir_path } {hifi_read_dir_path}")
+
+else:
+    print("\tError correction read dir was not found, skipping...")
+
+backup_stage_files("contig", results_dir_path, backup_dir_path,
+                ["*.fasta", ".fai", "*.gfa", "*.bed", "*.len", "*.cov", "*.lencov", "*.ids", "telomere"])
+
+backup_stage_files("hic_scaffolding", results_dir_path, backup_dir_path,
+                ["*.fasta", ".fai", "*.bed", "*.len", "*.assembly", "*.agp", "*.ids", "*.hic", "telomere",
                               "*.tab.gz", "*.png", "*.svg"])
 

cf2f8b0193015b5f24bf5f6b6cef1cff5c82ed2c
mahajrod <mahajrod@gmail.com>
2025-09-07 19:49:09 +0200

    Added simulate_pcr tool for insilico PCR

--- workflow/scripts/backup/gather_data_for_backup.py
+++ workflow/scripts/backup/gather_data_for_backup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 __author__ = 'mahajrod'
-
+import os
 import sys
+import shutil
 import argparse
 
 from pathlib import Path
@@ -18,4 +19,29 @@ parser.add_argument("-r", "--results_dir", action="store", dest="results_dir", r
 
 args = parser.parse_args()
 
+results_dir_path = Path(args.results_dir)
 
+
+backup_dir = results_dir_path / "backup/"
+os.makedirs(backup_dir, exist_ok=True)
+
+# backup QC
+qc_dir_path = results_dir_path / "qc/"
+shutil.copy(qc_dir_path, backup_dir)
+
+# backup kmer
+kmer_dir_path = results_dir_path / "kmer/"
+for folder_path in kmer_dir_path.glob("*"):
+    kmer_datatype = folder_path.name
+    filtered_kmer_dir_path = kmer_dir_path / kmer_datatype / "filtered/"
+    histo_file_path = list(filtered_kmer_dir_path.glob("*.histo"))[0]
+    genomescope_dir_path = list(filtered_kmer_dir_path.glob("genomescope"))[0]
+
+    backup_kmer_datatype_path = backup_dir / "kmer/" / kmer_datatype / "filtered/"
+    os.makedirs(backup_kmer_datatype_path, exist_ok=True)
+
+    print("Backuping kmer data...")
+    print(f"\tCopying {histo_file_path}...")
+    shutil.copy(histo_file_path, backup_kmer_datatype_path)
+    print(f"\tCopying {genomescope_dir_path}...")
+    shutil.copy(genomescope_dir_path, backup_kmer_datatype_path)

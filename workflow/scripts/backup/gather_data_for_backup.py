f55413a577fcd6be280b303d0641d643f95e2e56
mahajrod <mahajrod@gmail.com>
2026-08-22 01:58:41 +0200

    fArgSph1

--- workflow/scripts/backup/gather_data_for_backup.py
+++ workflow/scripts/backup/gather_data_for_backup.py
@@ -99,8 +99,8 @@ if kmer_dir_path.exists():
         filtered_kmer_dir_path = kmer_dir_path / kmer_datatype / "filtered/"
 
         backup_kmer_datatype_path = backup_dir_path / "kmer/" / kmer_datatype / "filtered/"
-        os.makedirs(filtered_kmer_dir_path, exist_ok=True)
-        print(backup_kmer_datatype_path)
+        os.makedirs(backup_kmer_datatype_path, exist_ok=True)
+        print(filtered_kmer_dir_path)
         histo_file_path = list(filtered_kmer_dir_path.glob("*.histo"))
         print(histo_file_path)
         if histo_file_path:
